3584fbc07d1eec334fd63b280f88bc5d59da2a06
Steven Hillman <stevenjhillman5@gmail.com>
2024-11-22 08:55:00 -0700

    add markview.nvim

--- init.lua
+++ init.lua
@@ -830,6 +830,15 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
