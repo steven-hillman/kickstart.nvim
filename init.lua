523618c5f82691510f424294a2d4548760528e63
Steven Hillman <stevenjhillman5@gmail.com>
2025-02-01 23:20:29 -0700

    create keymap for oil.nvim

--- init.lua
+++ init.lua
@@ -878,5 +878,7 @@ require('lazy').setup({
   },
 })
 
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
